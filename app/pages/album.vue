eca545ddfdcda67fb10a7768a0ca188e9f640730
ame <ame@abyss.moe>
2025-11-23 03:11:25 +0900

    feat: desgin base routes and add default pages

---
+++ app/pages/album.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1 class="text-2xl font-bold text-gray-900 mb-6">{{ $t('menu.album') }}</h1>
+    <div class="bg-white rounded-lg shadow p-6">
+      <p class="text-gray-600">Album page content</p>
+    </div>
+  </div>
+</template>
